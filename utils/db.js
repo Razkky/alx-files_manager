44db62a2576ca951d18bda71b8f5b411c8a4f09f
Abdul-Razak <muhammadjamiuabdulrazak@gmail.com>
2023-09-02 14:49:57 +0100

    initialize database

---
+++ utils/db.js
@@ -0,0 +1,53 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    this.db = null;
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'file_manager';
+    const dbUri = `mongodb://${host}:${port}/`;
+
+    // const dbUri = 'mongodb+srv://nodemognodb:aminah.aliyah@cluster0.rg3yamw.mongodb.net/?retryWrites=true&w=majority';
+    (async () => {
+      try {
+        const client = await MongoClient.connect(dbUri, { useUnifiedTopology: true });
+        this.db = client.db(database);
+        if (!(await this.collectionExists('users'))) {
+          await this.db.createCollection('users');
+        }
+        if (!(await this.collectionExists('files'))) {
+          await this.db.createCollection('files');
+        }
+        console.log('Connected to MongoDB');
+      } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+      }
+    })();
+  }
+
+  async collectionExists(collectionName) {
+    const collections = await this.db.listCollections().toArray();
+    return collections.some((collection) => collection.name === collectionName);
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  // User collection method, count number of document in collection
+  async nbUsers() {
+    console.log('getting users');
+    const countUser = await this.db.collection('users').countDocuments();
+    return countUser;
+  }
+
+  async nbFiles() {
+    console.log('getting files');
+    const countFiles = await this.db.collection('files').countDocuments();
+    return countFiles;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
