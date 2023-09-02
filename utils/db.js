a1317dc55a837cb24e02c5e3b1ae3b7b27ea1687
Abdul-Razak <muhammadjamiuabdulrazak@gmail.com>
2023-09-02 15:06:03 +0100

    change file contents

--- utils/db.js
+++ utils/db.js
@@ -3,47 +3,83 @@ const { MongoClient } = require('mongodb');
 class DBClient {
   constructor() {
     this.db = null;
+    // Use either env vars or defaults
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'file_manager';
-    const dbUri = `mongodb://${host}:${port}/`;
+    const database = process.env.DB_DATABASE || 'files_manager';
 
-    // const dbUri = 'mongodb+srv://nodemognodb:aminah.aliyah@cluster0.rg3yamw.mongodb.net/?retryWrites=true&w=majority';
-    (async () => {
-      try {
-        const client = await MongoClient.connect(dbUri, { useUnifiedTopology: true });
-        this.db = client.db(database);
-        if (!(await this.collectionExists('users'))) {
-          await this.db.createCollection('users');
-        }
-        if (!(await this.collectionExists('files'))) {
-          await this.db.createCollection('files');
-        }
-      } catch (err) {
-        console.error('Error connecting to MongoDB:', err);
-      }
-    })();
-  }
+    // MongoDB client connection
 
-  async collectionExists(collectionName) {
-    const collections = await this.db.listCollections().toArray();
-    return collections.some((collection) => collection.name === collectionName);
+    const url = `mongodb://${host}:${port}/`;
+
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, db) => {
+      if (err) console.log(err);
+      this.db = db.db(database);
+      this.db.createCollection('users');
+      this.db.createCollection('files');
+    });
   }
 
   isAlive() {
     return !!this.db;
   }
 
-  // User collection method, count number of document in collection
+  // users collection methods
+
   async nbUsers() {
-    const countUser = await this.db.collection('users').countDocuments();
-    return countUser;
+    const countUsers = await this.db.collection('users').countDocuments();
+    return countUsers;
   }
 
   async nbFiles() {
     const countFiles = await this.db.collection('files').countDocuments();
     return countFiles;
   }
+  // constructor() {
+  //   this.db = null;
+  //   const host = process.env.DB_HOST || 'localhost';
+  //   const port = process.env.DB_PORT || 27017;
+  //   const database = process.env.DB_DATABASE || 'file_manager';
+  //   // const dbUri = `mongodb://${host}:${port}/`;
+
+  //   const dbUri = 'mongodb+srv://nodemognodb:aminah.aliyah@cluster0.rg3yamw.mongodb.net/?retryWrites=true&w=majority';
+  //   (async () => {
+  //     try {
+  //       const client = await MongoClient.connect(dbUri, { useUnifiedTopology: true });
+  //       this.db = client.db(database);
+  // 	console.log('logging db', this.db);
+  //       if (!(await this.collectionExists('users'))) {
+  //         await this.db.createCollection('users');
+  //       }
+  //       if (!(await this.collectionExists('files'))) {
+  //         await this.db.createCollection('files');
+  //       }
+  //     } catch (err) {
+  //       console.error('Error connecting to MongoDB:', err);
+  //     }
+  //   })();
+  // }
+
+  // async collectionExists(collectionName) {
+  //   const collections = await this.db.listCollections().toArray();
+  //   return collections.some((collection) => collection.name === collectionName);
+  // }
+
+  // isAlive() {
+  //   return !!this.db;
+  // }
+
+  // User collection method, count number of document in collection
+  // async nbUsers() {
+  //   console.log(this.db)
+  //   const countUser = await this.db.collection('users').countDocuments();
+  //   return countUser;
+  // }
+
+  // async nbFiles() {
+  //   const countFiles = await this.db.collection('files').countDocuments();
+  //   return countFiles;
+  // }
 }
 
 const dbClient = new DBClient();
