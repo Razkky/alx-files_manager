76349c729b6f6613dbcabcff84f950204e94d771
Abdul-Razak <muhammadjamiuabdulrazak@gmail.com>
2023-09-03 11:33:21 +0100

    edit file

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,22 +5,22 @@ const getStatus = (req, res) => {
   try {
     const redis = redisClient.isAlive();
     const db = dbClient.isAlive();
-    res.status(200).send(({redis, db}));
-  } catch(error) {
+    res.status(200).send(({ redis, db }));
+  } catch (error) {
     console.log(error);
     res.status(500).send({ error: 'Server error' });
   }
 };
 
-const getStats =(req, res) => {
+const getStats = (req, res) => {
   try {
     const users = dbClient.nbUsers();
     const files = dbClient.nbFiles();
-    res.status(200).send({users, files});
-  } catch(error) {
+    res.status(200).send({ users, files });
+  } catch (error) {
     console.log(error);
     res.status(500).send({ error: 'Server error' });
   }
 };
 
-module.exports = {getStats, getStatus};
+module.exports = { getStats, getStatus };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const getStats = require('../controllers/AppController').getStats;
-const getStatus = require('../controllers/AppController').getStatus;
-
+const { getStats } = require('../controllers/AppController').getStats;
+const { getStatus } = require('../controllers/AppController').getStatus;
+const { postNew } = require('../controllers/UsersController').postNew
 
 const router = express.Router();
 
@@ -9,6 +9,6 @@ router.get('/status', getStatus);
 
 router.get('/stats', getStats);
 
+router.post('/users', postNew);
+
 module.exports = router;
-
-
